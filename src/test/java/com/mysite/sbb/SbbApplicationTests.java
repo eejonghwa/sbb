93f29d7166f8c5568e9a2766fb80f5dfe110fe9e
eejonghwa <teriyaki970326@gmail.com>
2023-10-25 14:12:47 +0900

    소설의 첫 줄 완성

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,35 @@
+package com.mysite.sbb;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Autowired
+	private QuestionService questionService;
+	@Test
+	void testJpa() {
+		for(int i = 1; i<=300; i++){
+			String subject = String.format("테스트 데이터입니다:[%03d]",i);
+			String content = "냉무";
+			this.questionService.create(subject,content);
+		}
+	}
+
+}
